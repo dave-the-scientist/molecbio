febff52e0facaa6f1b0693e000d1c1f96085ae6d
dave-the-scientist <daves.binf.tools@gmail.com>
2026-06-10 14:06:49 -0400

    restructured

--- src/molecbio/__init__.py
+++ src/molecbio/__init__.py
@@ -4,20 +4,15 @@ Several modules written to handle molecular biology tasks.
 -- Currently defines the following modules or packages:
    align -- Functions for dealing with sequence alignments.
    blosum -- the BLOSUM matrices as dictionaries.
-   pdb -- Functions for dealing with protein structure files.
+   Cluster -- Functions to align and measure PDB files.
+   parseblastxml -- Parse BLAST files.
+   PDB -- Functions for dealing with protein structure files.
+   phylo -- Functions to work with phylogenetic tree files.
    sequ -- Functions for DNA/protein sequences, alignments, etc.
-   rosetta -- A package to run and parse various rosetta programs.
-     Note that this package is not implicitly loaded with molecbio.
+
 """
-#__version__ = '0.6.1'
-#__author__ = 'Dave Curran (daves.binf.tools@gmail.com)'
-__all__ = ['PDB', 'Cluster', 'sequ', 'blosum', 'align', 'parseblastxml', 'phylo']
+__version__ = '0.6.2'
+__all__ = ['align', 'blosum', 'Cluster', 'parseblastxml', 'PDB', 'phylo', 'sequ']
 
 # when I rewrite this, include the alignment functionality from /home/dave/Desktop/work/p_multocida_slp_diversity/strain_prevalence/calculate_alignment_identity.py and the visualization functionality from /home/dave/Desktop/work/p_multocida_slp_diversity/strain_prevalence/analyze_hits.py and the blast sequence extraction from /home/dave/Desktop/work/p_multocida_slp_diversity/strain_prevalence/extract_sequences.py
 
-# try:
-#     from molecbio import Cluster
-# except ImportError:
-#     print("Could not import the Cluster module, so its functionality has been disabled. This is usually because the 'numpy' module could not be found.")
-#     Cluster = None
-# from molecbio import PDB, sequ, blosum, align, aligners
