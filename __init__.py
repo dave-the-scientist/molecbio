4cd29753140cbaed57cf63f658b3260055f802a9
dave-the-scientist <daves.binf.tools@gmail.com>
2026-06-10 12:55:34 -0400

    pyproject.toml

--- __init__.py
+++ __init__.py
@@ -9,15 +9,15 @@ Several modules written to handle molecular biology tasks.
    rosetta -- A package to run and parse various rosetta programs.
      Note that this package is not implicitly loaded with molecbio.
 """
-__version__ = '0.6.1'
-__author__ = 'Dave Curran (daves.binf.tools@gmail.com)'
-__all__ = ['PDB', 'Cluster', 'sequ', 'blosum', 'align', 'rosetta', 'aligners', 'phylo']
+#__version__ = '0.6.1'
+#__author__ = 'Dave Curran (daves.binf.tools@gmail.com)'
+__all__ = ['PDB', 'Cluster', 'sequ', 'blosum', 'align', 'parseblastxml', 'phylo']
 
 # when I rewrite this, include the alignment functionality from /home/dave/Desktop/work/p_multocida_slp_diversity/strain_prevalence/calculate_alignment_identity.py and the visualization functionality from /home/dave/Desktop/work/p_multocida_slp_diversity/strain_prevalence/analyze_hits.py and the blast sequence extraction from /home/dave/Desktop/work/p_multocida_slp_diversity/strain_prevalence/extract_sequences.py
 
-try:
-    from molecbio import Cluster
-except ImportError:
-    print("Could not import the Cluster module, so its functionality has been disabled. This is usually because the 'numpy' module could not be found.")
-    Cluster = None
-from molecbio import PDB, sequ, blosum, align, aligners
+# try:
+#     from molecbio import Cluster
+# except ImportError:
+#     print("Could not import the Cluster module, so its functionality has been disabled. This is usually because the 'numpy' module could not be found.")
+#     Cluster = None
+# from molecbio import PDB, sequ, blosum, align, aligners
